e836726b42876671313b70b69c67944a315154c1
Ragav <external.ragavendiran.saravanan@sky.de>
2024-04-07 12:28:42 +0530

    comments

--- problem/1.Array/Cummulative.js
+++ problem/1.Array/Cummulative.js
@@ -1,23 +1,21 @@
 /* The cumulative sum of an array at index i is defined as the sum of all elements of the array from index 0 to index i.
-
 Examples:
+*********
 Initial Array: [1, 2, 3, 4]
 Cumulative Sum: [1, 3, 6, 10]
-
+*************************************************************************************************************************
 Initial Array: [1, 1, 1, 1, 1]
 Cumulative Sum: [1, 2, 3, 4, 5]
-
+*************************************************************************************************************************
 Initial Array: [1, 3, 5, 7, 9]
 Cumulative Sum: [1, 4, 9, 16, 25]
-
 */
 
 
 /*
 T --> O(N)
-S --> O(1)
+S --> O(N)
 */
-
 function cumulative_sum(arr=[]) {
     let cummulativeSumArray = [];
     let sum = 0;
